e2f36b84ef0eef2b9c9ba308cd0884d07a50018b
iwosw <168121902+iwosw@users.noreply.github.com>
2026-05-17 02:05:54 +0300

    Add new decorative plants

--- src/main/java/com/iwosw/vivariumlibera/registry/ModBlocks.java
+++ src/main/java/com/iwosw/vivariumlibera/registry/ModBlocks.java
@@ -4,6 +4,7 @@ import com.iwosw.vivariumlibera.VivariumLibera;
 import com.iwosw.vivariumlibera.block.SimplePlantBlock;
 import com.iwosw.vivariumlibera.block.StackablePlantBlock;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.PinkPetalsBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -18,9 +19,17 @@ public final class ModBlocks {
     public static final DeferredBlock<StackablePlantBlock> ST_JOHNS_WORT = registerStackablePlant("st_johns_wort");
     public static final DeferredBlock<StackablePlantBlock> DATURA = registerStackablePlant("datura");
     public static final DeferredBlock<StackablePlantBlock> FIREWEED = registerStackablePlant("fireweed");
+    public static final DeferredBlock<StackablePlantBlock> CHICORY = registerStackablePlant("chicory");
 
     public static final DeferredBlock<SimplePlantBlock> CALAMUS = registerSimplePlant("calamus");
     public static final DeferredBlock<SimplePlantBlock> CATTAIL = registerSimplePlant("cattail");
+    public static final DeferredBlock<SimplePlantBlock> CALENDULA = registerSimplePlant("calendula");
+    public static final DeferredBlock<PinkPetalsBlock> WOOD_SORREL_RED = registerMeadowFlower("wood_sorrel_red");
+    public static final DeferredBlock<PinkPetalsBlock> WOOD_SORREL_YELLOW = registerMeadowFlower("wood_sorrel_yellow");
+    public static final DeferredBlock<SimplePlantBlock> YARROW_WHITE = registerSimplePlant("yarrow_white");
+    public static final DeferredBlock<SimplePlantBlock> YARROW_RED = registerSimplePlant("yarrow_red");
+    public static final DeferredBlock<SimplePlantBlock> YARROW_PINK = registerSimplePlant("yarrow_pink");
+    public static final DeferredBlock<SimplePlantBlock> YARROW_YELLOW = registerSimplePlant("yarrow_yellow");
 
     private ModBlocks() {
     }
@@ -37,7 +46,15 @@ public final class ModBlocks {
         return BLOCKS.register(name, () -> new SimplePlantBlock(plantProperties()));
     }
 
+    private static DeferredBlock<PinkPetalsBlock> registerMeadowFlower(String name) {
+        return BLOCKS.register(name, () -> new PinkPetalsBlock(meadowFlowerProperties()));
+    }
+
     private static BlockBehaviour.Properties plantProperties() {
         return BlockBehaviour.Properties.ofFullCopy(Blocks.DANDELION);
+    }
+
+    private static BlockBehaviour.Properties meadowFlowerProperties() {
+        return BlockBehaviour.Properties.ofFullCopy(Blocks.PINK_PETALS);
     }
 }
