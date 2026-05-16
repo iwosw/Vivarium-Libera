016d3cc88d81d9428d07cd1ef156bb3e93ff830f
IWOSS <oivan7086@gmail.com>
2026-05-17 02:05:54 +0300

    Add new decorative plants

--- src/main/java/com/iwosw/vivariumlibera/registry/ModItems.java
+++ src/main/java/com/iwosw/vivariumlibera/registry/ModItems.java
@@ -19,8 +19,16 @@ public final class ModItems {
     public static final DeferredItem<BlockItem> ST_JOHNS_WORT_ITEM = registerBlockItem("st_johns_wort", ModBlocks.ST_JOHNS_WORT);
     public static final DeferredItem<BlockItem> DATURA_ITEM = registerBlockItem("datura", ModBlocks.DATURA);
     public static final DeferredItem<BlockItem> FIREWEED_ITEM = registerBlockItem("fireweed", ModBlocks.FIREWEED);
+    public static final DeferredItem<BlockItem> CHICORY_ITEM = registerBlockItem("chicory", ModBlocks.CHICORY);
     public static final DeferredItem<BlockItem> CALAMUS_ITEM = registerBlockItem("calamus", ModBlocks.CALAMUS);
     public static final DeferredItem<BlockItem> CATTAIL_ITEM = registerBlockItem("cattail", ModBlocks.CATTAIL);
+    public static final DeferredItem<BlockItem> CALENDULA_ITEM = registerBlockItem("calendula", ModBlocks.CALENDULA);
+    public static final DeferredItem<BlockItem> WOOD_SORREL_RED_ITEM = registerBlockItem("wood_sorrel_red", ModBlocks.WOOD_SORREL_RED);
+    public static final DeferredItem<BlockItem> WOOD_SORREL_YELLOW_ITEM = registerBlockItem("wood_sorrel_yellow", ModBlocks.WOOD_SORREL_YELLOW);
+    public static final DeferredItem<BlockItem> YARROW_WHITE_ITEM = registerBlockItem("yarrow_white", ModBlocks.YARROW_WHITE);
+    public static final DeferredItem<BlockItem> YARROW_RED_ITEM = registerBlockItem("yarrow_red", ModBlocks.YARROW_RED);
+    public static final DeferredItem<BlockItem> YARROW_PINK_ITEM = registerBlockItem("yarrow_pink", ModBlocks.YARROW_PINK);
+    public static final DeferredItem<BlockItem> YARROW_YELLOW_ITEM = registerBlockItem("yarrow_yellow", ModBlocks.YARROW_YELLOW);
 
     public static final DeferredItem<Item> KNIFE = ITEMS.register("knife", () -> new Item(new Item.Properties()));
 
@@ -31,8 +39,16 @@ public final class ModItems {
             ST_JOHNS_WORT_ITEM,
             DATURA_ITEM,
             FIREWEED_ITEM,
+            CHICORY_ITEM,
             CALAMUS_ITEM,
-            CATTAIL_ITEM);
+            CATTAIL_ITEM,
+            CALENDULA_ITEM,
+            WOOD_SORREL_RED_ITEM,
+            WOOD_SORREL_YELLOW_ITEM,
+            YARROW_WHITE_ITEM,
+            YARROW_RED_ITEM,
+            YARROW_PINK_ITEM,
+            YARROW_YELLOW_ITEM);
 
     private ModItems() {
     }
