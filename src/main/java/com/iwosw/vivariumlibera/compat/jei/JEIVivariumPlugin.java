b9431e6b0f767220aebc1bfbd1ee5e541f1c1388
iwosw <168121902+iwosw@users.noreply.github.com>
2026-07-20 22:27:57 +0300

    feat(alchemy): add decoction recipes and JEI category
    Six brewing recipes plus a JEI category that shows ingredients, the
    required liquid, brew time, and whether strong fire is needed.

--- src/main/java/com/iwosw/vivariumlibera/compat/jei/JEIVivariumPlugin.java
+++ src/main/java/com/iwosw/vivariumlibera/compat/jei/JEIVivariumPlugin.java
@@ -1,13 +1,21 @@
 package com.iwosw.vivariumlibera.compat.jei;
 
 import com.iwosw.vivariumlibera.VivariumLibera;
+import com.iwosw.vivariumlibera.recipe.DecoctionRecipe;
+import com.iwosw.vivariumlibera.registry.ModItems;
+import com.iwosw.vivariumlibera.registry.ModRecipes;
+import java.util.List;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
 
 @JeiPlugin
 public class JEIVivariumPlugin implements IModPlugin {
@@ -20,12 +28,17 @@ public class JEIVivariumPlugin implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        // Here we will register the Alchemy recipe category when ready
+        registration.addRecipeCategories(new DecoctionRecipeCategory(registration.getJeiHelpers().getGuiHelper()));
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        // Here we will load and register recipes
+        ClientLevel level = Minecraft.getInstance().level;
+        if (level == null) {
+            return;
+        }
+        List<RecipeHolder<DecoctionRecipe>> recipes = level.getRecipeManager().getAllRecipesFor(ModRecipes.DECOCTION_TYPE.get());
+        registration.addRecipes(DecoctionRecipeCategory.TYPE, recipes);
     }
 
     @Override
@@ -35,6 +48,7 @@ public class JEIVivariumPlugin implements IModPlugin {
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-        // Here we will register the Alchemy Table block as a catalyst for alchemy recipes
+        registration.addRecipeCatalyst(new ItemStack(ModItems.ALCHEMY_TABLE_ITEM.get()), DecoctionRecipeCategory.TYPE);
+        registration.addRecipeCatalyst(new ItemStack(ModItems.ALCHEMY_CAULDRON.get()), DecoctionRecipeCategory.TYPE);
     }
 }
